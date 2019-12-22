fc09fa5561e9f869f7cc2336682c8aad1102cae4
purin52002 <purin7635@gmail.com>
2019-12-22 22:01:15 +0900

    list image

--- application/views/sandbox.py
+++ application/views/sandbox.py
@@ -1,25 +1,35 @@
 from flask import Blueprint, render_template, request
-from application.models import User
 from pathlib import Path
+from application.services.compare_image import image_path_dict, to_base64
+from PIL import Image
+
+
 sandbox_bp = Blueprint('sandbox_bp', __name__)
 
-
-images_dir_path = Path(r'./application/static/images')
 image_dict_list = [
     {
         'path': 'static/images/'+image_path.name,
-        'name': image_path.stem
+        'name': name
     }
-    for image_path in images_dir_path.iterdir()
+    for name, image_path in image_path_dict.items()
 ]
+
 
 @sandbox_bp.route('/image')
 def image():
-    image_dict_list.extend(image_dict_list)
+    # image_dict_list.extend(image_dict_list)
     print(image_dict_list)
     return render_template('image.html', image_dict_list=image_dict_list)
 
 
 @sandbox_bp.route('/select_image', methods=['POST'])
 def select_image():
-    return request.form['select']
+    name = request.form['select']
+
+    image = Image.open(image_path_dict[name]).convert('RGB')
+
+    return render_template('compare.html',
+                           left_image=to_base64(image),
+                           right_image=to_base64(image),
+                           count=100)
+
