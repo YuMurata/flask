d6791c6bdb2c3896a1a3045a6111656bf2016392
purin52002 <purin7635@gmail.com>
2020-01-19 22:53:05 +0900

    Update optimize.py

--- enhancer/Enhancer/optimize.py
+++ enhancer/Enhancer/optimize.py
@@ -3,20 +3,32 @@ from .image_generator import ImageGenerator
 from .preference_predictor import PreferencePredictor
 from .ParameterOptimizer.ParameterOptimizer import Optimizer
 from .config import DirectoryPath
+from .logger import Logger
+
+logger = Logger(__name__)
 
 
 def optimize_image():
-    for user_dir in DirectoryPath.weights.iterdir()
-    user_name = user_dir
-    model_weight_file_path = None
+    for user_dir in DirectoryPath.users.iterdir():
+        if not user_dir.is_dir():
+            logger.warn(f'{str(user_dir)} is not directory !')
+            continue
 
-    generator = ImageGenerator(image_path)
-    image_path = None
-    param_list, logbook = \
-        Optimizer(PreferencePredictor(model_weight_file_path),
-                  generator,
-                  ParamDecoder()).optimize(30, param_list_num=1)
+        user_name = user_dir.name
 
-    save_file_path = DirectoryPath.optimize
-    generator.generate(param_list[0]).save()
-    print(param_list)
+        weight_dir = user_dir/'weights'
+        weight_file_path = weight_dir/'weight.h5'
+
+        optimize_dir = user_dir/'optimizes'
+        optimize_dir.mkdir(exist_ok=True, parents=True)
+
+        generator = ImageGenerator(image_path)
+        image_path = None
+        param_list, logbook = \
+            Optimizer(PreferencePredictor(model_weight_file_path),
+                      generator,
+                      ParamDecoder()).optimize(30, param_list_num=1)
+
+        save_file_path = DirectoryPath.optimize
+        generator.generate(param_list[0]).save()
+        print(param_list)
