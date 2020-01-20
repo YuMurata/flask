91b4e83a2dbda1350c2d3ba96073d61e0cde4a82
purin52002 <purin7635@gmail.com>
2020-01-20 12:22:59 +0900

    fix

--- enhancer/Enhancer/preference_predictor.py
+++ enhancer/Enhancer/preference_predictor.py
@@ -1,12 +1,12 @@
-from ParameterOptimizer.ParameterOptimizer import PredictModel
-from ImageRankNet.ImageRankNet import RankNet
+from .ParameterOptimizer.ParameterOptimizer import PredictModel
+from .ImageRankNet.ImageRankNet import RankNet
 from PIL.Image import Image
-from .config import IMAGE_SHAPE
+from .config import ImageInfo
 
 
 class PreferencePredictor(PredictModel):
     def __init__(self, model_weight_file_path: str):
-        self.model = RankNet(IMAGE_SHAPE)
+        self.model = RankNet(ImageInfo.shape)
         self.model.load(model_weight_file_path)
 
     def predict(self, image: Image) -> float:
