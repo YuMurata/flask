04c5c498e324626afa159f40740b044e0aeeeb38
purin52002 <purin7635@gmail.com>
2020-02-14 15:20:42 +0900

    update

--- web/application/views/__init__.py
+++ web/application/views/__init__.py
@@ -1,3 +1,3 @@
-from . import (auth, compare, index, sandbox, scored_data, image)
+from . import (auth, compare, index, scored_data, image)
 bp_list = [auth.auth_bp, compare.compare_bp, index.index_bp,
-           sandbox.sandbox_bp, scored_data.scored_data_bp, image.image_bp]
+           scored_data.scored_data_bp, image.image_bp]
